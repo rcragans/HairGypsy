74289e9e4f2c0e4bc03c22c2d3b9668ea1c6c7ca
Robert Ragans <robertragans@Roberts-MacBook-Pro-2.local>
2019-01-07 11:00:16 -0500

    map function updated

---
+++ contact.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#getDirections").click(function(){
+        var address = prompt("Enter Starting Address")
+        var hairGypsyLoc = {lat: 33.856500, lng: -84.344134}
+        var apiKey = "AIzaSyCegr0g-FBKeJBGSLGm2ozYmPPw4UVk65o"
+        $("#map").html(`<iframe id="directions" width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/directions?origin=${address}&destination=salon%20lofts%20brrokhaven&key=AIzaSyCegr0g-FBKeJBGSLGm2ozYmPPw4UVk65o" allowfullscreen></iframe>`)
+    })
+})
